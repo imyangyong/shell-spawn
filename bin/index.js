1bc82748d6a487115c981089beb2ec5802c8b381
杨勇 <1994101600@qq.com>
2019-11-19 21:32:18 +0800

    feat(v2.0.0): support exec shell file

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,14 @@
 'use strict';
 
+const util = require('util');
 const { spawn } = require('child_process');
+const execFile = util.promisify(require('child_process').execFile);
 
-function shellSpawn (command = '', options = {}) {
-  if (options.verbose) {
-    console.log("shell-spawn: about to spawn " + command);
-  }
-  
+function execCommand(command, options) {
   if (Array.isArray(command)) {
     command = command.join(';')
   }
-  
-  options = Object.assign({ env: process.env, cwd: process.cwd() }, options);
-  
-  const shell = process.platform === 'win32' ? { cmd: 'cmd', arg: '/C' } : { cmd: 'sh', arg: '-c' };
+  const shell = process.platform === 'win32' ? {cmd: 'cmd', arg: '/C'} : {cmd: 'sh', arg: '-c'};
   
   let child;
   
@@ -24,26 +19,26 @@ function shellSpawn (command = '', options = {}) {
   }
   
   return new Promise((resolve, reject) => {
-    let output = "";
+    let output = '';
     
     function toStdErr(data) {
       output += data;
       if (options.verbose) {
-        console.warn("shell-spawn: error: " + data.toString());
+        console.warn('shell-spawn: error: ' + data.toString());
       }
     }
     
     function toStdOut(data) {
       output += data;
       if (options.verbose) {
-        console.log("shell-spawn: output: " + data.toString());
+        console.log('shell-spawn: output: ' + data.toString());
       }
     }
     
     child.stdout.on('data', toStdOut);
     child.stderr.on('data', toStdErr);
     child.on('error', reject);
-    child.on('close', function(code) {
+    child.on('close', function (code) {
       if (code === 0) {
         resolve(output);
       } else {
@@ -54,6 +49,35 @@ function shellSpawn (command = '', options = {}) {
       }
     });
   });
+}
+
+async function execShellFile(filePath) {
+  try {
+    var res = await execFile(filePath, []);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+  const {error, stdout, stderr} = res;
+  return new Promise((resolve, reject) => {
+    if (error || stderr) {
+      reject(error || stderr);
+    }
+    resolve(stdout);
+  })
+}
+
+function shellSpawn(commandOrFilePath = '', options = {}) {
+  if (options.verbose) {
+    console.log('shell-spawn: about to spawn ' + commandOrFilePath);
+  }
+  
+  options = Object.assign({env: process.env, cwd: process.cwd()}, options);
+  
+  if (options.file) {
+    return execShellFile(commandOrFilePath)
+  } else {
+    return execCommand(commandOrFilePath, options)
+  }
 };
 
 module.exports = shellSpawn;
